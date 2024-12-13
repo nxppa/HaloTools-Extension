3b4ae5bd97d8ebd044239e7075682489768de3a8
Nappa <079nappa173@gmail.com>
2024-12-14 07:37:35 +1100

    updates

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-document.querySelector('.unlock-button').addEventListener('click', async () => {
-    const password = document.querySelector('.input-box').value;
+//34a75bef08004f789e548e709171c8822af6aec25b32260a169d2ac384746ecf8a01b6eb0ff56653d1e4fb5361e2f10495edea7ef56ef8d42884fea2b061e605
+document.addEventListener('DOMContentLoaded', () => {
 
-    // Send the password to the server for verification
-    const response = await fetch('https://your-api.com/authenticate', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ password }), // Send password in JSON format
+    document.querySelector('.unlock-button').addEventListener('click', async () => { //TODO make rate limits
+        console.log("pressed unlock")
+        const password = document.querySelector('.input-box').value;
+        const Req = `https://bayharbour.boats/authenticate?key=${password}`
+        console.log("req: ", Req)
+        const response = await fetch(Req);
+        const result = await response.json();
+        if (response.ok) {
+            console.log('Authentication successful:', result);
+            document.body.classList.add('hidden')
+            setTimeout(() => {
+                window.location.href = "Pages/MainPage.html";
+            }, 500)
+
+        } else {
+            const box = document.querySelector('.input-box');
+            box.style.boxShadow = '0 0 20px rgba(255, 0, 0, 0.8)';
+
+            // Fade back to original shadow after 1 second
+            setTimeout(() => {
+                box.style.boxShadow = '0 0 15px 0px #4b4b4b';
+            }, 1000); // Match the transition duration
+        }
     });
-
-    const result = await response.json();
-
-    if (response.ok) {
-        console.log('Authentication successful:', result);
-        // Proceed to authenticated area
-    } else {
-        console.error('Authentication failed:', result);
-        alert('Invalid password!');
-    }
-});
+})

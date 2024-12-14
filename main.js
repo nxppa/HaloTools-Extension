a77ce636657110e2f830dd039b9753f372fd11e4
Nappa <079nappa173@gmail.com>
2024-12-15 00:34:34 +1100

    u

--- main.js
+++ main.js
@@ -1,8 +1,52 @@
 //34a75bef08004f789e548e709171c8822af6aec25b32260a169d2ac384746ecf8a01b6eb0ff56653d1e4fb5361e2f10495edea7ef56ef8d42884fea2b061e605
+const AuthTimeMins = 8
+window.addEventListener('unload', () => {
+    const Token = localStorage.getItem('session_token')
+    if (Token) {
+        const req = `https://bayharbour.boats/validate?token=${Token}`
+        fetch(req)
+    }
+ })
+ 
 
 
-
+ 
 document.addEventListener('DOMContentLoaded', () => {
+    function ClearToken() {
+        localStorage.removeItem('session_token');
+        document.cookie = 'session_token=; Max-Age=0; path=/';
+    }
+    async function SetToken(Token) {
+        localStorage.setItem('session_token', Token);
+        document.cookie = `session_token=${Token}; HttpOnly; Secure; Max-Age=${AuthTimeMins * 60};`
+    }
+    async function OpenMainPage() {
+        document.body.classList.add('hidden')
+        InputBox.style.boxShadow = '0 0 20px rgba(0, 255, 0, 0.8)';
+        setTimeout(() => {
+            window.location.href = "Pages/Main/MainPage.html";
+        }, 500)
+    }
+    async function ValidateOnLoad() {
+        const Token = localStorage.getItem('session_token')
+        if (Token) {
+            console.log(Token)
+            const req = `https://bayharbour.boats/validate?token=${Token}`
+            console.log("validate req: ", req)
+            const Response = await fetch(req)
+            console.log(Response)
+            const result = await Response.json();
+            if (result.success) {
+                SetToken(result.token)
+                console.log(result)
+                OpenMainPage()
+            } else {
+            }
+        } else {
+            console.log("no token")
+        }
+    }
+    ValidateOnLoad()
     const InputBox = document.querySelector('.input-box');
     const QuestionMark = document.getElementById("QuestionMark")
     QuestionMark.addEventListener('click', () => {
@@ -10,20 +54,17 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     InputBox.focus()
-    async function ProccessEntry(){ //TODO make ratelimit
+    async function ProccessEntry() { //TODO make ratelimit
         const password = InputBox.value;
         const Req = `https://bayharbour.boats/authenticate?key=${password}`
         console.log("req: ", Req)
         const response = await fetch(Req);
         const result = await response.json();
         if (response.ok) {
-            document.cookie = `session_token=${token}; HttpOnly; Secure; Max-Age=480;`
+            SetToken(result.token)
+            console.log(result.token)
             console.log('Authentication successful:', result);
-            document.body.classList.add('hidden')
-            InputBox.style.boxShadow = '0 0 20px rgba(0, 255, 0, 0.8)';
-            setTimeout(() => {
-                window.location.href = "Pages/MainPage.html";
-            }, 500)
+            OpenMainPage()
         } else {
             InputBox.style.boxShadow = '0 0 20px rgba(255, 0, 0, 0.8)';
             setTimeout(() => {
@@ -31,7 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }, 1000)
         }
     }
-    
+
     InputBox.addEventListener('keydown', (event) => {
         if (event.key === 'Enter') {
             ProccessEntry()

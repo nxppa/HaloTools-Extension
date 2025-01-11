af60bb679e0be59bbb53b9c9cee482b4901bbc23
Nappa <079nappa173@gmail.com>
2025-01-12 06:50:00 +1100

    u

--- Pages/Copy/script.js
+++ Pages/Copy/script.js
@@ -47,10 +47,10 @@ async function post(URL, body) {
         body: JSON.stringify(body),
     });
 }
-function SetDictionaryItem(Item, Dictionary){
+function SetDictionaryItem(Item, Dictionary) {
     localStorage.setItem(Item, JSON.stringify(Dictionary))
 }
-function GetDictionaryItem(Item){
+function GetDictionaryItem(Item) {
     const UserData = localStorage.getItem(Item)
     const DataParsed = JSON.parse(UserData)
     return DataParsed
@@ -88,7 +88,7 @@ function is24HoursSince(epochTime) {
     return elapsedTime >= 24 * 60 * 60 * 1000
 }
 
-function ChangeWalletAddress(EditFrameVis, New, Data){
+function ChangeWalletAddress(EditFrameVis, New, Data) {
     const Original = EditFrameVis.Wallet.id
     EditFrameVis.Wallet.id = New
     const UserData = GetDictionaryItem("UserData")
@@ -105,7 +105,7 @@ function ChangeWalletAddress(EditFrameVis, New, Data){
         data => {
             console.log("IsValid: ", data.IsValid)
             let Colour = null
-            if (data.IsValid){
+            if (data.IsValid) {
                 const UserData = GetDictionaryItem("UserData")
                 UserData.Targets[New].Valid = true
                 SetDictionaryItem("UserData", UserData)
@@ -125,7 +125,7 @@ function ChangeWalletAddress(EditFrameVis, New, Data){
     return UserData
 }
 
-function SetWalletDetails(Frame, Details){
+function SetWalletDetails(Frame, Details) {
     const id = Frame.Wallet.id
     const UserData = GetDictionaryItem("UserData")
     UserData.Targets[id] = Details
@@ -174,6 +174,8 @@ window.addEventListener('load', () => {
     document.body.classList.add('visible');
     const EditFrame = document.createElement('div');
     EditFrame.className = "EditFrame"
+    const PreviousTransactionsFrame = document.createElement("div")
+    PreviousTransactionsFrame.className = "PreviousTransactionsFrame"
 
     for (const k in Parameters) {
         const PerameterHolder = document.createElement("div")
@@ -205,11 +207,11 @@ window.addEventListener('load', () => {
         EditFrame.classList.toggle('hidden');
         EditFrameVisible.Wallet.style.boxShadow = '0 0 10px 0px #4b4b4b';
         let VariablesParsed = {}
-        for (const k in EditFrameKids){
+        for (const k in EditFrameKids) {
             const Frame = EditFrameKids[k]
             console.log("parsed: ", k)
             const Actual = ParamAliasToID[k]
-            if (Actual){
+            if (Actual) {
                 VariablesParsed[Actual] = Frame.Input.value
             }
         }
@@ -220,21 +222,23 @@ window.addEventListener('load', () => {
         VariablesParsed.Halted = UserData.Targets[EditFrameVisible.Wallet.id].Halted
         EditFrameVisible.AliasLabel.textContent = VariablesParsed.Alias
         ActiveMapping[EditFrameVisible.Wallet.id] = false
-        if (EditFrameVisible.Wallet.id != NewWallet){
+        if (EditFrameVisible.Wallet.id != NewWallet) {
             const Colour = "grey"
             EditFrameVisible.WalletLabel.innerHTML = `${shorthandString(NewWallet, 3, 5, 5)}\u2009<span style="color: ${Colour};">\u25C9</span>`;
             ChangeWalletAddress(EditFrameVisible, NewWallet, VariablesParsed)
         } else {
             SetWalletDetails(EditFrameVisible, VariablesParsed)
         }
-        EditFrameVisible = false 
+        EditFrameVisible = false
 
     })
 
     let LastIndexedAt = null
     let PreviousWalletDiv = null
     EditFrame.classList.toggle('hidden');
+    PreviousTransactionsFrame.classList.toggle('hidden');
     let EditFrameVisible = false
+    let PreviousTransactionsFrameVisible  = false
     const ScrollBox = document.getElementById("scroll-box")
     let WalletDivDex = 0
     function AddWalletDiv(WalletAddress, DataBaseData, Client) {
@@ -244,82 +248,79 @@ window.addEventListener('load', () => {
         const children = ScrollBox.children;
         const secondToLast = children[children.length - 1];
 
-        
+
         Wallet.className = 'WalletHolder';
         Wallet.id = WalletAddress;
         ScrollBox.insertBefore(Wallet, secondToLast)
-        
+
         const IconsHolder = document.createElement('div');
         IconsHolder.className = "IconsHolder"
         Wallet.appendChild(IconsHolder)
-        
-        
 
         const Details = document.createElement('input');
         Details.type = "image"
         Details.src = DetailsAsset
         Details.className = "WalletIcon"
         IconsHolder.appendChild(Details)
-        
+
 
         const Pen = document.createElement('input');
         Pen.type = "image"
         Pen.src = PenAsset
         Pen.className = "WalletIcon"
         IconsHolder.appendChild(Pen)
-        
-        
+
+
         const Trash = document.createElement('input');
         Trash.type = "image"
         Trash.src = TrashAsset
         Trash.className = "WalletIcon"
         IconsHolder.appendChild(Trash)
-        
+
         const PauseStatus = document.createElement('input');
         PauseStatus.type = "image"
         PauseStatus.className = "WalletIcon"
         IconsHolder.appendChild(PauseStatus)
-        
+
         const TransactionInfo = document.createElement('div')
         TransactionInfo.className = "TransactionInfo"
         Wallet.appendChild(TransactionInfo)
-        
+
 
         const TimeStamp = document.createElement("span")
         TimeStamp.id = "TimeStamp"
         //TODO Make this a function to update most recent transaction
         //TODO make this check the target wallet's "Most recent transaction" element
-        const epochTime = Math.floor(1736539079 - Math.random()*100000) 
+        const epochTime = Math.floor(Date.now() - Math.random() * 100000)
         let TimeStr = null
-        if (is24HoursSince(epochTime)){
+        if (is24HoursSince(epochTime)) {
             const TimeParsed = convertEpochToDate(epochTime, false)
             TimeStr = `${TimeParsed}`
 
         } else {
-            const TimeParsed = convertEpochToLocalTimeWithPeriod(epochTime) 
+            const TimeParsed = convertEpochToLocalTimeWithPeriod(epochTime)
             TimeStr = `${TimeParsed.time}\u2009${TimeParsed.period}`
             //TODO make it say month/day depending on locale
         }
-        TimeStamp.innerHTML =  `<span>${TimeStr}</span>`
+        TimeStamp.innerHTML = `<span>${TimeStr}</span>`
         TimeStamp.style.color = "rgba(150, 150, 150)"
         TransactionInfo.appendChild(TimeStamp)
         const LastTradeType = document.createElement("span")
         LastTradeType.id = "LTT"
-        
-        const LTT =  Math.random() > 0.5 ? "BOUGHT" : "SOLD"
+
+        const LTT = Math.random() > 0.5 ? "BOUGHT" : "SOLD"
         const LastTransClr = LTT == "BOUGHT" ? "green" : "red"
         const CLR = LastTransClr == "green" ? 'rgba(0, 255, 0, 0.2)' : "rgba(255, 0, 0, 0.2)"
-        LastTradeType.innerHTML =  `<span style="color:${LastTransClr}">${LTT}</span>`
+        LastTradeType.innerHTML = `<span style="color:${LastTransClr}">${LTT}</span>`
         LastTradeType.style.backgroundColor = CLR;
         LastTradeType.style.borderRadius = '4px'
         LastTradeType.style.border = `2px solid ${CLR}`;
-        LastTradeType.style.padding = '4px 8px'; 
-        console.log(GetDictionaryItem("UserData"), Wallet.id)
-        if (!GetDictionaryItem("UserData").Targets[Wallet.id] || GetDictionaryItem("UserData").Targets[Wallet.id].Valid != true){ 
+        LastTradeType.style.padding = '4px 8px';
+        if (!GetDictionaryItem("UserData").Targets[Wallet.id] || GetDictionaryItem("UserData").Targets[Wallet.id].Valid != true) {
             TransactionInfo.classList.toggle('hidden');
         }
         TransactionInfo.appendChild(LastTradeType)
-        
+
         const WalletInfoHolder = document.createElement('div');
         WalletInfoHolder.className = "WalletInfoHolder"
         Wallet.appendChild(WalletInfoHolder)
@@ -375,44 +376,103 @@ window.addEventListener('load', () => {
                 AddressName.innerHTML = `${shorthandString(Wallet.id, 3, 5, 5)}\u2009<span style="color: ${Colour};">\u25C9</span>`;
             }
         })
+        // Pen button logic
         Pen.addEventListener("click", () => {
-            const InsertAt = children[getElementIndex(Wallet) + 1]
-            ScrollBox.insertBefore(EditFrame, InsertAt)
-            if (PreviousWalletDiv) {
-                PreviousWalletDiv.style.boxShadow = '0 0 10px 0px #4b4b4b'
+            // Hide PreviousTransactionsFrame if visible
+            if (PreviousTransactionsFrameVisible) {
+                PreviousTransactionsFrame.classList.add('hidden');
+                PreviousTransactionsFrameVisible = false;
+                PreviousWalletDiv.style.boxShadow = '0 0 10px 0px #4b4b4b';
             }
+
+            const InsertAt = children[getElementIndex(Wallet) + 1];
+            ScrollBox.insertBefore(EditFrame, InsertAt);
+
+            if (PreviousWalletDiv) {
+                PreviousWalletDiv.style.boxShadow = '0 0 10px 0px #4b4b4b';
+            }
+
             if (EditFrameVisible) {
-                if (CurrentWalletDiv == LastIndexedAt) {
+                if (CurrentWalletDiv === LastIndexedAt) {
                     EditFrame.classList.toggle('hidden');
-                    EditFrameVisible = false
+                    EditFrameVisible = false;
                     Wallet.style.boxShadow = '0 0 10px 0px #4b4b4b';
                 } else {
                     Wallet.style.boxShadow = '0 0 20px #FFBD59';
                 }
             } else {
                 EditFrame.classList.toggle('hidden');
-                EditFrameVisible = {Wallet: Wallet, WalletLabel: AddressName, AliasLabel: AliasName, OriginalAccount: Wallet.id, PauseIcon: PauseStatus}
+                EditFrameVisible = {
+                    Wallet: Wallet,
+                    WalletLabel: AddressName,
+                    AliasLabel: AliasName,
+                    OriginalAccount: Wallet.id,
+                    PauseIcon: PauseStatus
+                };
                 Wallet.style.boxShadow = '0 0 20px #FFBD59';
-
             }
-            PreviousWalletDiv = Wallet
-            LastIndexedAt = CurrentWalletDiv
-            const UserData = GetDictionaryItem("UserData")
-            const CurrentTargets = UserData.Targets
-            const CurrentTarget = CurrentTargets[Wallet.id]
-            if (EditFrameVisible) {
-                EditFrameKids["Wallet"].Input.value = Wallet.id
-                for (const k in DataBaseData) {
-                    const Alias = ParamIDToAlias[k]
-                    if (Parameters[Alias]) {
-                        console.log("Alias: ", Alias)
-                        EditFrameKids[Alias].Input.value = CurrentTarget[k]
-                    }
 
+            PreviousWalletDiv = Wallet;
+            LastIndexedAt = CurrentWalletDiv;
+
+            const UserData = GetDictionaryItem("UserData");
+            const CurrentTargets = UserData.Targets;
+            const CurrentTarget = CurrentTargets[Wallet.id];
+
+            if (EditFrameVisible) {
+                EditFrameKids["Wallet"].Input.value = Wallet.id;
+                for (const k in DataBaseData) {
+                    const Alias = ParamIDToAlias[k];
+                    if (Parameters[Alias]) {
+                        console.log("Alias: ", Alias);
+                        EditFrameKids[Alias].Input.value = CurrentTarget[k];
+                    }
                 }
             }
-        })
 
+            PauseStatus.src = DataBaseData.Halted ? UnpauseAssetIcon : PauseAssetIcon;
+            ActiveMapping[Wallet.id] = !DataBaseData.Halted;
+        });
+
+        // Details button logic
+        Details.addEventListener("click", () => {
+            // Hide EditFrame if visible
+            if (EditFrameVisible) {
+                EditFrame.classList.add('hidden');
+                EditFrameVisible = false;
+                PreviousWalletDiv.style.boxShadow = '0 0 10px 0px #4b4b4b';
+            }
+
+            const InsertAt = children[getElementIndex(Wallet) + 1];
+            ScrollBox.insertBefore(PreviousTransactionsFrame, InsertAt);
+
+            if (PreviousWalletDiv) {
+                PreviousWalletDiv.style.boxShadow = '0 0 10px 0px #4b4b4b';
+            }
+
+            if (PreviousTransactionsFrameVisible) {
+                if (CurrentWalletDiv === LastIndexedAt) {
+                    PreviousTransactionsFrame.classList.toggle('hidden');
+                    PreviousTransactionsFrameVisible = false;
+                    Wallet.style.boxShadow = '0 0 10px 0px #4b4b4b';
+                } else {
+                    Wallet.style.boxShadow = '0 0 20px #FFBD59';
+                }
+            } else {
+                PreviousTransactionsFrame.classList.toggle('hidden');
+                PreviousTransactionsFrameVisible = {
+                    Wallet: Wallet,
+                    WalletLabel: AddressName,
+                    AliasLabel: AliasName,
+                    OriginalAccount: Wallet.id
+                };
+                Wallet.style.boxShadow = '0 0 20px #FFBD59';
+            }
+
+            PreviousWalletDiv = Wallet;
+            LastIndexedAt = CurrentWalletDiv;
+
+        });
 
         PauseStatus.src = DataBaseData.Halted ? UnpauseAssetIcon : PauseAssetIcon
         ActiveMapping[Wallet.id] = !DataBaseData.Halted
@@ -438,7 +498,7 @@ window.addEventListener('load', () => {
         const Token = localStorage.getItem('session_token')
         const URL = `https://bayharbour.boats/newWallet?session_token=${Token}&account=${NewName}`
         post(URL, BaseWalletTemplate)
-        
+
     })
 
 });
